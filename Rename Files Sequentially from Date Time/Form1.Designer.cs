121ec067a0e2290865b43f476507746a70840d8b
Riya Thapa <thapari@deakin.edu.au>
2021-12-22 16:24:35 +1100

    Add project files.

---
+++ Rename Files Sequentially from Date Time/Form1.Designer.cs
@@ -0,0 +1,108 @@
+﻿
+namespace Rename_Files_Sequentially_from_Date_Time
+{
+    partial class frmRenameSequentially
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.txtSelectedDir = new System.Windows.Forms.TextBox();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnOK = new System.Windows.Forms.Button();
+            this.fbdSelectedDir = new System.Windows.Forms.FolderBrowserDialog();
+            this.SuspendLayout();
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(535, 12);
+            this.btnBrowse.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(100, 28);
+            this.btnBrowse.TabIndex = 0;
+            this.btnBrowse.Text = "Browse...";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // txtSelectedDir
+            // 
+            this.txtSelectedDir.Location = new System.Drawing.Point(16, 15);
+            this.txtSelectedDir.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtSelectedDir.Name = "txtSelectedDir";
+            this.txtSelectedDir.Size = new System.Drawing.Size(509, 22);
+            this.txtSelectedDir.TabIndex = 1;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(535, 48);
+            this.btnCancel.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(100, 28);
+            this.btnCancel.TabIndex = 2;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            this.btnCancel.Click += new System.EventHandler(this.btnCancel_Click);
+            // 
+            // btnOK
+            // 
+            this.btnOK.Location = new System.Drawing.Point(427, 47);
+            this.btnOK.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnOK.Name = "btnOK";
+            this.btnOK.Size = new System.Drawing.Size(100, 28);
+            this.btnOK.TabIndex = 3;
+            this.btnOK.Text = "OK";
+            this.btnOK.UseVisualStyleBackColor = true;
+            this.btnOK.Click += new System.EventHandler(this.btnOK_Click);
+            // 
+            // frmRenameSequentially
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(651, 90);
+            this.Controls.Add(this.btnOK);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.txtSelectedDir);
+            this.Controls.Add(this.btnBrowse);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.Name = "frmRenameSequentially";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Select directory";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox txtSelectedDir;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnOK;
+        private System.Windows.Forms.FolderBrowserDialog fbdSelectedDir;
+    }
+}
+
---
+++ Rename Files Sequentially from Date Time/Form1.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.IO;
+
+namespace Rename_Files_Sequentially_from_Date_Time
+{
+    public partial class frmRenameSequentially : Form
+    {
+        public frmRenameSequentially()
+        {
+            InitializeComponent();
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            fbdSelectedDir.ShowDialog();
+            txtSelectedDir.Text = fbdSelectedDir.SelectedPath;
+        }
+
+        private void btnCancel_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void btnOK_Click(object sender, EventArgs e)
+        {
+            if (Directory.Exists(fbdSelectedDir.SelectedPath))
+            {
+                this.Enabled = false;
+
+                // Create an enumerable list of files and order by creation date
+                DirectoryInfo di = new DirectoryInfo(fbdSelectedDir.SelectedPath);
+                IEnumerable<FileInfo> iEnFi = di.EnumerateFiles();
+                iEnFi = iEnFi.OrderBy(x => x.CreationTime);
+ 
+                try
+                {                    
+                    int fileCount = iEnFi.Count();
+                    int currentFileNameCount = 1;
+
+                    // Rename each file in the directory sequentially by chronological order
+                    foreach (FileInfo fi in iEnFi)
+                    {
+                        // Get current file's extension
+                        string fileExt = '.' + fi.Name.Split('.')[fi.Name.Split('.').Length - 1];
+
+                        double currentProgressPercent = currentFileNameCount / fileCount * 100;
+
+                        // Log current progress to console window
+                        Console.WriteLine("Progress: {0}% ({1}/{2})", currentProgressPercent, currentFileNameCount, fileCount);
+                        Console.WriteLine("{0} to {1} ({2})", fi.Name, currentFileNameCount, fi.CreationTime);
+
+                        // Rename the file
+                        File.Move(fi.FullName, fbdSelectedDir.SelectedPath + '\\' + currentFileNameCount + fileExt);
+
+                        currentFileNameCount++;
+                    }
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+
+                this.Enabled = true;
+            }
+        }
+    }
+}
